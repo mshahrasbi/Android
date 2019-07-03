43cf84c2716f1e138e522e976c0e218b3c87ed90
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-07-02 23:24:08 -0700

    Egg Timer App

--- Section5/EggTimer/app/src/main/java/com/shahrasbi/eggtimer/MainActivity.java
+++ Section5/EggTimer/app/src/main/java/com/shahrasbi/eggtimer/MainActivity.java
@@ -1,13 +1,101 @@
 package com.shahrasbi.eggtimer;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private SeekBar timerSeekBar;
+    private Button goButton;
+    private Boolean counterIsActive = false;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.countdownTextView);
+        goButton = findViewById(R.id.goButton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void buttonClicked(View view) {
+        Log.i("Button Pressed", "Nice!");
+
+        if (counterIsActive) {
+            resetTimer();
+        } else {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+
+                    Log.i("Finished", "Timer all done");
+
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mplayer.start();
+
+                    resetTimer();
+                }
+            }.start();
+        }
+    }
+
+    private void updateTimer(int progress){
+        int minutes = progress / 60;
+        int seconds = progress - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+        if (seconds <= 9){
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
+    private void resetTimer() {
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("GO!");
+        counterIsActive = false;
     }
 }
