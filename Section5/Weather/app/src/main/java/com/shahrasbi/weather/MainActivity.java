e5c12e6579f5cf84d8c1e6a56a0156efc4544599
Mohammad Shahrasbi <mohammad.shahrasbi@opusinspection.com>
2019-08-09 14:45:54 -0700

    weather app

--- Section5/Weather/app/src/main/java/com/shahrasbi/weather/MainActivity.java
+++ Section5/Weather/app/src/main/java/com/shahrasbi/weather/MainActivity.java
@@ -1,9 +1,15 @@
-package com.shahrasbi.a11_json;
+package com.shahrasbi.weather;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,8 +18,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText cityEditText;
+    private TextView resultTextView;
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -42,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
                 return result;
             } catch(Exception ex) {
                 ex.printStackTrace();
+
+                Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT).show();
+
                 return null;
             }
         }
@@ -62,25 +77,56 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("weather content", weatherInfo);
 
                 JSONArray   arr = new JSONArray(weatherInfo);
+                String message = "";
+
                 for (int i = 0; i < arr.length(); i++) {
                     JSONObject jsonPart = arr.getJSONObject(i);
 
-                    Log.i("main", jsonPart.getString("main"));
-                    Log.i("description", jsonPart.getString("description"));
+                    //Log.i("main", jsonPart.getString("main"));
+                    //Log.i("description", jsonPart.getString("description"));
+
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if (!main.equals("") && !description.equals("")) {
+                        message += main + ": " + description + "\r\n";
+                    }
+                }
+
+                if (!message.equals("")) {
+                    resultTextView.setText(message);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT).show();
                 }
             } catch(Exception ex) {
+                Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT).show();
                 ex.printStackTrace();
             }
         }
     }
 
+    public void getWeather(View view) {
+        try {
+            String encodedCityName = URLEncoder.encode(this.cityEditText.getText().toString(), "UTF-8");
+
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&APPID=badc4defd6239cd368f5a861a4fca951");
+
+            // get keyboard hide
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(this.cityEditText.getWindowToken(), 0);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Could not find weather :(", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadTask task = new DownloadTask();
-        task.execute("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=badc4defd6239cd368f5a861a4fca951");
+        this.cityEditText = findViewById(R.id.cityEditText);
+        this.resultTextView = findViewById(R.id.resultTextView);
     }
 }
